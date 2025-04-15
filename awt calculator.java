91a25e64a93ce7f6b4e5ecf9e97903a0545f7054
A534-glitch <sadharsh306@gmail.com>
2025-04-15 21:43:27 +0530

    Add files via upload

---
+++ awt calculator.java
@@ -0,0 +1,152 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class SimpleCalculator extends Frame implements ActionListener {
+    TextField t1, t2, result;
+    Button add, sub, mul, div;
+
+    SimpleCalculator() {
+        // Create components
+        t1 = new TextField(10);
+        t2 = new TextField(10);
+        result = new TextField(20);
+        result.setEditable(false);
+
+        add = new Button("+");
+        sub = new Button("-");
+        mul = new Button("*");
+        div = new Button("/");
+
+        // Add listeners
+        add.addActionListener(this);
+        sub.addActionListener(this);
+        mul.addActionListener(this);
+        div.addActionListener(this);
+
+        // Layout
+        setLayout(new FlowLayout());
+
+        add(new Label("Number 1:"));
+        add(t1);
+        add(new Label("Number 2:"));
+        add(t2);
+        add(add); add(sub); add(mul); add(div);
+        add(new Label("Result:"));
+        add(result);
+
+        // Frame settings
+        setSize(300, 200);
+        setTitle("Simple Calculator");
+        setVisible(true);
+
+        // Close window
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            double num1 = Double.parseDouble(t1.getText());
+            double num2 = Double.parseDouble(t2.getText());
+            double res = 0;
+
+            if (e.getSource() == add) res = num1 + num2;
+            else if (e.getSource() == sub) res = num1 - num2;
+            else if (e.getSource() == mul) res = num1 * num2;
+            else if (e.getSource() == div) {
+                if (num2 == 0) {
+                    result.setText("Cannot divide by zero");
+                    return;
+                }
+                res = num1 / num2;
+            }
+
+            result.setText("Result: " + res);
+        } catch (NumberFormatException ex) {
+            result.setText("Invalid input");
+        }
+    }
+
+    public static void main(String[] args) {
+        new SimpleCalculator();
+    }
+}
+import java.awt.*;
+import java.awt.event.*;
+
+public class SimpleCalculator extends Frame implements ActionListener {
+    TextField t1, t2, result;
+    Button add, sub, mul, div;
+
+    SimpleCalculator() {
+        // Create components
+        t1 = new TextField(10);
+        t2 = new TextField(10);
+        result = new TextField(20);
+        result.setEditable(false);
+
+        add = new Button("+");
+        sub = new Button("-");
+        mul = new Button("*");
+        div = new Button("/");
+
+        // Add listeners
+        add.addActionListener(this);
+        sub.addActionListener(this);
+        mul.addActionListener(this);
+        div.addActionListener(this);
+
+        // Layout
+        setLayout(new FlowLayout());
+
+        add(new Label("Number 1:"));
+        add(t1);
+        add(new Label("Number 2:"));
+        add(t2);
+        add(add); add(sub); add(mul); add(div);
+        add(new Label("Result:"));
+        add(result);
+
+        // Frame settings
+        setSize(300, 200);
+        setTitle("Simple Calculator");
+        setVisible(true);
+
+        // Close window
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            double num1 = Double.parseDouble(t1.getText());
+            double num2 = Double.parseDouble(t2.getText());
+            double res = 0;
+
+            if (e.getSource() == add) res = num1 + num2;
+            else if (e.getSource() == sub) res = num1 - num2;
+            else if (e.getSource() == mul) res = num1 * num2;
+            else if (e.getSource() == div) {
+                if (num2 == 0) {
+                    result.setText("Cannot divide by zero");
+                    return;
+                }
+                res = num1 / num2;
+            }
+
+            result.setText("Result: " + res);
+        } catch (NumberFormatException ex) {
+            result.setText("Invalid input");
+        }
+    }
+
+    public static void main(String[] args) {
+        new SimpleCalculator();
+    }
+}
